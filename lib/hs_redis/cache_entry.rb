8bb8d79e90934816f7c674aaa2a8b7432492d868
hkilis-hs <h.elklisly@hungerstation.com>
2020-08-30 02:57:22 +0200

    feat(hs_redis): remove mandatory blocks and callbacks

---
+++ lib/hs_redis/cache_entry.rb
@@ -0,0 +1,21 @@
+module HsRedis
+  class CacheEntry
+    attr_reader :value
+
+    def self.serialize(value)
+      return if value.nil?
+      Marshal.dump(self.new(value))
+    end
+
+    def self.parse(cache_entry)
+      return if cache_entry.nil?
+      Marshal.load(cache_entry).value rescue cache_entry
+    end
+
+    private
+
+    def initialize(value)
+      @value = value
+    end
+  end
+end
